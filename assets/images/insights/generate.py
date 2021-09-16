3158e76040615f4e7613790c421b37856e771955
harshasridhar <sridharsha598@gmail.com>
2021-09-16 15:30:57 +0000

    updated README

--- assets/images/insights/generate.py
+++ assets/images/insights/generate.py
@@ -3,7 +3,7 @@ readme=open('README.md','w')
 filenames=os.listdir()
 text='# Insights from Data\n<table>\n<tr><th>Metric</th><th>Image</th></tr>\n'
 for filename in filenames:
-    if 'jpg' in filename or 'png' in filename or 'PNG' in filename:
+    if 'jpg' in filename or 'png' in filename or 'PNG' in filename or 'gif' in filename:
         text += '<tr><td>{}</td><td><img src="{}"/></td></tr>\n'.format(filename.split('.')[0],filename)
 text+='</table>'
 readme.write(text)
